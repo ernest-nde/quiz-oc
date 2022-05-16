6b149957dd31630ad9683d97692fd89b3d28aee2
ernest-nde <ernestmekontso@gmail.com>
2022-05-17 01:21:16 +0200

    Soumission du projet final avec backend pret à l'utilisation

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ This repertory content all backend files like :
 - App creation with Express,
 - Data Structure shema : done with Moogone that is you to connect our app to this project MongoDB database,
 - The use of the CORS (Cross Origin Resource Sharing ), which is used by default, blocks HTTP calls between different servers, thus preventing malicious requests from accessing sensitive resources (In our case localhost:3000 for the backend and localhost:4200 for the front end)
+
+So in Backend repertory we have differents files
+- .gitignore: its in charge of ignoring some of files or repertories during pushing the project to remote repository
+- server.js : server file, in charge creating and exports of the app
+- app.js : content all project's routes and strucure
+- models : which is the repertory that has the Product.js that representing our data structure.
+
+# HAVE A FUN!!!
+
+7Exi9FbFfws-gR3
--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,79 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://menders:7Exi9FbFfws-gR3@cluster0.6la0a.mongodb.net/quiz-oc?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true 
+    })
+  .then(() => console.log('Connexion à la BD Quiz-OC réussie !'))
+  .catch(() => console.log('Connexion à la BD Quiz-OC échouée !'));
+
+
+const Produit = require('./models/Product');
+
 
 app.use((req, res, next) => {
-    console.log('server oK!');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
-app.use((req, res) => {
-    res.status(200).json({message: 'Le serveur est bien ouvert!'});
+app.use(bodyParser.json());
+
+// Enregistrement d'un nouveau produit
+app.post('/api/products', (req, res, next) => {
+    delete req.body._id;
+    const produit = new Produit({
+        ...req.body
+    });
+    produit.save()
+    .then(() => res.status(201).json({message: 'produit bien enregistré!'}))
+    .catch(function(error) {
+        res.status(400).json({error});
+    });
+});
+
+// Récupération d'un produit spécifique
+app.post('/api/products/:id', (req, res, next) => {
+    Produit.findOne({ _id: req.params.id })
+    .then(function(thing) {
+        res.status(200).json(thing)
+    })
+    .catch(function(error) {
+        res.status(404).json({error});
+    });
+});
+
+// Modification d'un produit spécifique
+app.put('/api/products/:id', (req, res, next) => {
+    Produit.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id})
+    .then(() => {
+        res.status(200).json({message: 'Produit modifié!'});
+    })
+    .catch(function(error) {
+        res.status(404).json({error});
+    });
+});
+
+// Récupération d'un produit spécifique
+app.delete('/api/products/:id', (req, res, next) => {
+    Produit.deleteOne({ _id: req.params.id })
+    .then(() => {
+        res.status(200).json({message: 'Produit supprimé!'});
+    })
+    .catch(function(error) {
+        res.status(404).json({error});
+    });
+});
+
+// Liste de tous les produits disponible
+app.get('/api/products', (req, res, next) => {
+    Produit.find()
+    .then(produits => res.status(200).json(produits))
+    .catch(error => res.status(400).json({error}));
 });
 
 module.exports = app;
